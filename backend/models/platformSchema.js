d1613155277782814e33c63d582bebcd75bd1eec
Kumar Anand <kranand9430@gmail.com>
2026-09-02 12:11:04 +0530

    platform schema defined

---
+++ backend/models/platformSchema.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const platformSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      unique: true,
+    },
+
+    platforms: {
+      leetcodeHandle: { type: String, default: '' },
+      codeforcesHandle: { type: String, default: '' },
+      codechefHandle: { type: String, default: '' },
+      githubHandle: { type: String, default: '' },
+      hackerrankHandle: { type: String, default: '' },
+    },
+
+    codeforcesData: {
+      type: mongoose.Schema.Types.Mixed,
+      default: () => ({}),
+    },
+    leetcodeData: {
+      type: mongoose.Schema.Types.Mixed,
+      default: () => ({}),
+    },
+    codechefData: {
+      type: mongoose.Schema.Types.Mixed,
+      default: () => ({}),
+    },
+    githubData: {
+      type: mongoose.Schema.Types.Mixed,
+      default: () => ({}),
+    },
+    hackerrankData: {
+      type: mongoose.Schema.Types.Mixed,
+      default: () => ({}),
+    },
+  },
+);
+
+export default mongoose.model('Platform', platformSchema);
